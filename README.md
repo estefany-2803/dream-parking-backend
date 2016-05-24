720906a81d5125db316c2dc7cd5d76f106d54f50
H2OOPS <27hdmr@gmail.com>
2016-05-24 16:59:09 -0500

    Avance Parametrización MySQL

--- README.md
+++ README.md
@@ -4,17 +4,18 @@
 En la carpeta del proyecto crear un archivo llamado `.env` y dentro de este definir las siguientes variables:
 
 ```bash
+MYSQL_HOST='localhost' # Host donde corre MySQL
 MYSQL_USER='' # Definir Usuario de MySQL
 MYSQL_PASS='' # Definir Password a la base de datos
 MYSQL_DB='' # Definir a que base de datos se va a conectar la aplicación
 ```
 
 #### 2. Crear la base de datos y crear tablas
-En el directorio `models` estan las definiciones SQL de todas las tablas. Es necesario validar que la base de datos está correctamente parametrizada.
+En el directorio `modelos` estan las definiciones SQL de todas las tablas. Es necesario validar que la base de datos está correctamente parametrizada.
 
 #### 3. Poner en marcha.
 En la carpeta del proyecto ejecutar:
 
 ```bash
-[Dream_Park] $ npm start
+[Dream_Parking] $ npm start
 ```
--- app.js
+++ app.js
@@ -2,7 +2,42 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+const ENV = require('dotenv').load(); // Esto carga el archivo .env con las variables de entorno.
+	
+/** Seteamos la base de datos */
+var Sequelize = require('sequelize');
 
+const VARS = {
+	MYSQL_HOST: process.env.MYSQL_HOST,
+	MYSQL_USER: process.env.MYSQL_USER,
+	MYSQL_PASS: process.env.MYSQL_PASS,
+	MYSQL_DB: process.env.MYSQL_DB
+};
+
+/**
+ *	Inicializamos Sequelize.
+ *	Sequelize es un ORM o libreria de mapeo objeto-relacional,
+ *	Que permite manipular SQL a un alto nivel, evitando así escribir sentencias SQL puras,
+ *	y brinda un API del lenguaje muy sencillo de entender.	
+ *	
+ *	La declaración GLOBAL.DATABASE hace referencia a una variable global, en el momento de la ejecución.
+ *	Lo que traduce a que el objeto de coneccion es accesible desde cualquier parte de la aplicacion, sin
+ *	necesidad de estar importando un fichero. 
+ *	
+ *	@info http://docs.sequelizejs.com/en/latest/docs/getting-started/#setting-up-a-connection
+ */
+GLOBAL.DATABASE = new Sequelize(VARS.MYSQL_DB, VARS.MYSQL_USER, VARS.MYSQL_PASS {
+	host: VARS.MYSQL_HOST,
+	dialect: 'mysql'
+})
+
+/**
+ *	Parametrizamos el servidor.
+ *	Express es una libreria/framework minimalista, que facilita el desarrollo
+ *	de aplicaciones basadas en el protocolo HTTP.
+ *	
+ *	@info http://expressjs.com
+ */
 const app = express();
 const rutas = require('./routes.js');
 
